17766a071176cd7822e861734485b5ff2a1ea434
thomas.blank <blanky0230@gmail.com>
2020-12-09 09:28:55 +0100

    Day9 in ugly but solved :)

---
+++ day9/XmasCracker.ts
@@ -0,0 +1,80 @@
+import { readFileSync } from 'fs';
+
+const isValidInSequence = (
+    sequence: Array<number>,
+    newNumber: number,
+    cursor: number,
+    preamble: number
+) =>
+    sequence
+        .slice(cursor - preamble, cursor)
+        .filter(
+            (v, idx, seq) => seq.includes(newNumber - v) && newNumber - v !== v
+        ).length > 0;
+
+const findCorruptedNumber = (
+    sequence: Array<number>,
+    preamble: number
+): number => {
+    let idx = preamble;
+    while (
+        isValidInSequence(sequence, sequence[idx], idx, preamble) &&
+        idx < sequence.length - 1
+    ) {
+        idx++;
+    }
+    return sequence[idx];
+};
+
+const findSumSequence = (originalSequence: Array<number>, preamble: number) => {
+    const corruptNumber = findCorruptedNumber(originalSequence, preamble);
+    let start = 0;
+    let end = 0;
+    let curSum = -1;
+    while (curSum !== corruptNumber) {
+        curSum = originalSequence
+            .slice(start, end)
+            .reduce((c, cu) => c + cu, 0);
+        if (curSum === corruptNumber) {
+            return originalSequence.slice(start, end);
+        }
+        if (curSum > corruptNumber) {
+            start++;
+        } else {
+            end++;
+        }
+    }
+    return originalSequence.slice(start, end);
+};
+
+const data = readFileSync('input.txt', 'utf-8')
+    .split('\n')
+    .filter((x) => x)
+    .map((n) => Number.parseInt(n, 10));
+
+console.log(findCorruptedNumber(data, 25));
+// const testData = [
+//     35,
+//     20,
+//     15,
+//     25,
+//     47,
+//     40,
+//     62,
+//     55,
+//     65,
+//     95,
+//     102,
+//     117,
+//     150,
+//     182,
+//     127,
+//     219,
+//     299,
+//     277,
+//     309,
+//     576,
+// ];
+const part2 = findSumSequence(data, 25).sort((a, b) => a - b);
+console.log(part2[0] + part2[part2.length - 1]);
+// console.log(findSumSequence(testData, 5));
