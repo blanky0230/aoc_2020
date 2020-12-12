9becaca447d642fbca79c4c1a26f03b661b68567
thomas.blank <blanky0230@gmail.com>
2020-12-12 14:42:07 +0100

    day 11 part 2 <3

--- day11/GameOfSeating.ts
+++ day11/GameOfSeating.ts
@@ -11,7 +11,7 @@ const seat = (position: Vector, state: SeatState): Seat => ({
     state,
 });
 
-const neighbours = (seat: Seat, world: Map<string, Seat>): Seat[] => {
+const neighboursSorrounding = (seat: Seat, world: Map<string, Seat>): Seat[] => {
     const neighbours = new Array<Seat>();
     const [centerX, centerY] = seat.position;
     for (let i = centerX - 1; i <= centerX + 1; i++) {
@@ -30,18 +30,50 @@ const neighbours = (seat: Seat, world: Map<string, Seat>): Seat[] => {
     return neighbours;
 };
 
-const mustToggle = (seat: Seat, world: Map<string, Seat>): boolean => {
+const raycastNeighbours = (seat: Seat, world: Map<string, Seat>): Seat[] => {
+    const directions: Vector[] = [
+        [1, 0],
+        [0, 1],
+        [1, 1],
+        [-1, 1],
+        [1, -1],
+        [-1, 0],
+        [0, -1],
+        [-1, -1],
+    ];
+    return directions
+        .map((d: Vector) => {
+            let check = addVectors(seat.position, d);
+            let cursor = JSON.stringify(check);
+            while (world.has(cursor)) {
+                if (world.get(cursor)?.state !== '.') {
+                    return world.get(cursor);
+                }
+                check = addVectors(check, d);
+                cursor = JSON.stringify(check);
+            }
+            return null;
+        })
+        .filter((n) => n) as Seat[];
+};
+
+const addVectors = (vecA: Vector, vecB: Vector): Vector => [
+    vecA[0] + vecB[0],
+    vecA[1] + vecB[1],
+];
+
+const mustToggle = (seat: Seat, world: Map<string, Seat>, neighboursFunc: (seat: Seat, world: Map<string, Seat>) => Seat[], dieThreshold: number): boolean => {
     switch (seat.state) {
         case '.':
             return false;
         case '#':
             return (
-                neighbours(seat, world).filter((s) => s.state === '#').length >=
-                4
+                neighboursFunc(seat, world).filter((s) => s.state === '#').length >=
+                dieThreshold
             );
         case 'L':
             return (
-                neighbours(seat, world).filter((s) => s.state === '#')
+                neighboursFunc(seat, world).filter((s) => s.state === '#')
                     .length === 0
             );
     }
@@ -53,9 +85,9 @@ const cellsAsArray = (cellMap: Map<string, Seat>): Seat[] => {
     return result;
 };
 
-const update = (world: Map<string, Seat>): Map<string, Seat> =>
+const update = (world: Map<string, Seat>, neighboursFunc: (seat: Seat, world: Map<string, Seat>) => Seat[], dieThreshold: number): Map<string, Seat> =>
     cellsAsArray(world)
-        .filter((seat) => mustToggle(seat, world))
+        .filter((seat) => mustToggle(seat, world, neighboursFunc, dieThreshold))
         .map(
             (seat) =>
                 ({ ...seat, state: seat.state === '#' ? 'L' : '#' } as Seat)
@@ -90,27 +122,42 @@ const x = async () => {
             .join('\n')
     );
 
-    const world = text.split('\n').reduce((world, line, idx) => {
-        const latitude = idx;
-        line.split('').map((value, longitude) => {
-            const position = [latitude, longitude] as Vector;
-            const state = value as SeatState;
-            world.set(JSON.stringify(position), seat(position, state));
-        });
-        return world;
-    }, new Map<string, Seat>());
+    const world = text
+        .split('\n')
+        .reduce((world: Map<string, Seat>, line: string, idx: number) => {
+            const latitude = idx;
+            line.split('').map((value: string, longitude: number) => {
+                const position = [latitude, longitude] as Vector;
+                const state = value as SeatState;
+                world.set(JSON.stringify(position), seat(position, state));
+            });
+            return world;
+        }, new Map<string, Seat>());
 
     const partOne = () => {
         let lastHash = printWorld(world);
         let currHash = '';
         let map = world;
         while (lastHash !== currHash) {
-            map = update(map);
+            map = update(map, neighboursSorrounding, 4);
+            lastHash = currHash;
+            currHash = printWorld(map);
+        }
+        console.log(cellsAsArray(world).filter((s) => s.state === '#').length);
+    };
+
+    const partTwo = () => {
+        let lastHash = printWorld(world);
+        let currHash = '';
+        let map = world;
+        while (lastHash !== currHash) {
+            map = update(map, raycastNeighbours, 5);
             lastHash = currHash;
             currHash = printWorld(map);
         }
         console.log(cellsAsArray(world).filter((s) => s.state === '#').length);
     };
     partOne();
+    partTwo();
 };
 x();
