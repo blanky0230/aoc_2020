e174325e9555e6dfabf5614d677a2fedcb4d91b8
thomas.blank <blanky0230@gmail.com>
2020-12-18 14:11:15 +0100

    day16 <3

--- day16/TicketTranslation.ts
+++ day16/TicketTranslation.ts
@@ -8,22 +8,112 @@ const main = async () => {
     const rules = notes[0]
         .split('\n')
         .filter((x) => x)
-        .map((x) => /(\d+)-(\d+) or (\d+)-(\d+)/.exec(x.trim()))
-        .map((x) => x?.slice(-4).map((n) => Number.parseInt(n, 10)))
-        .map((bounds) => (n: number) =>
-            (bounds![0] <= n && bounds![1] >= n) ||
-            (bounds![2] <= n && bounds![3] >= n)
+        .map((x) => {
+            const name = x.split(':')[0].trim();
+            const bounds = /(\d+)-(\d+) or (\d+)-(\d+)/
+                .exec(x.trim())
+                ?.slice(-4)
+                .map((n) => Number.parseInt(n, 10));
+            return { name, bounds };
+        })
+        .map(({ name, bounds }) => ({
+            name,
+            fun: (n: number) =>
+                (bounds![0] <= n && bounds![1] >= n) ||
+                (bounds![2] <= n && bounds![3] >= n),
+        }));
+
+    const nearbyyTicketsFlat = notes[2]
+        .split('\n')
+        .slice(1)
+        .map((x) => x.split(',').map((x) => x.trim()))
+        .flat(1)
+        .map((x) => Number.parseInt(x, 10));
+
+    console.log(
+        `part 1: ${nearbyyTicketsFlat
+            .filter((t) => rules.every((r) => !r.fun(t)))
+            .reduce((x, y) => x + y, 0)}`
+    );
+
+    const nearbyTickets = notes[2]
+        .split('\n')
+        .slice(1)
+        .map((x) => x.split(',').map((x) => Number.parseInt(x.trim(), 10)));
+
+    const validNearbyTickets = nearbyTickets.filter(
+        (t: number[]) =>
+            t.filter((num) => rules.every((r) => !r.fun(num))).length === 0
+    );
+
+    const rulesToIdx = rules.reduce((carry, rule) => {
+        const validOn = validNearbyTickets
+            .map((ticket) =>
+                ticket.reduce(
+                    (carry, curr, currIdx) =>
+                        rule.fun(curr) ? [...carry, currIdx] : carry,
+                    [] as number[]
+                )
+            )
+            .flat(1);
+
+        const alwaysValidOnIdx = new Set(
+            validOn.filter(
+                (x) =>
+                    validOn.filter((y) => y === x).length ===
+                    validNearbyTickets.length
+            )
         );
-    const nearbyyTickets = notes[2]
+        return carry.set(rule.name, alwaysValidOnIdx);
+    }, new Map<string, Set<number>>());
+
+    const nextTargetForRemoval = (
+        map: Map<string, Set<number>>,
+        alreadyCleared: Set<string>
+    ): string | null => {
+        let answer = null;
+        map.forEach((value, key) => {
+            if (value.size === 1 && !alreadyCleared.has(key)) {
+                answer = key;
+            }
+        });
+        return answer;
+    };
+
+    let clearedKeys = new Set<string>();
+    let nextKeyToClearFrom = nextTargetForRemoval(rulesToIdx, clearedKeys);
+    while (nextKeyToClearFrom !== null) {
+        const val = Array.from(rulesToIdx.get(nextKeyToClearFrom)!.values())[0];
+        // console.log(`deleting ${val} from all rules other than ${nextKeyToClearFrom}`);
+        const targets = Array.from(rulesToIdx.keys()).filter(
+            (k) => k !== nextKeyToClearFrom
+        );
+        targets.map((k) => {
+            const set = rulesToIdx.get(k)!;
+            set.delete(val);
+            rulesToIdx.set(k, set);
+        });
+        clearedKeys.add(nextKeyToClearFrom);
+        nextKeyToClearFrom = nextTargetForRemoval(rulesToIdx, clearedKeys);
+    }
+
+    const depatureKeys = Array.from(rulesToIdx.keys()).filter((k) =>
+        k.split(' ').includes('departure')
+    );
+    const departureIndicies = depatureKeys.map(
+        (k) => Array.from(rulesToIdx.get(k)!)[0]
+    );
+    const myTicket = notes[1]
         .split('\n')
         .slice(1)
         .map((x) => x.split(',').map((x) => x.trim()))
         .flat(1)
         .map((x) => Number.parseInt(x, 10));
     console.log(
-        nearbyyTickets
-            .filter((t) => rules.every((r) => !r(t)))
-            .reduce((x, y) => x + y, 0)
+        `part 2: ${departureIndicies.reduce(
+            (carry, idx) => carry * myTicket[idx],
+            1
+        )}`
     );
 };
 
